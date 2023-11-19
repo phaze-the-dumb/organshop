f22704bb608cb45c1ff5e67eea55af4af47b6477
phaze-the-dumb <phaze@duck.com>
2023-11-19 21:55:31 +0000

    pain

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,61 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import * as Realm from 'realm-web';
+
+import homepage from './home.page';
 
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
+	MONGODB_KEY: string;
+	MONGODB_ID: string;
 }
 
+let links: any;
+
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(req: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		if(!links){
+			let app = new Realm.App({ id: env.MONGODB_ID });
+			let user = await app.logIn(Realm.Credentials.apiKey(env.MONGODB_KEY));
+
+			let mongo = user.mongoClient('mongodb-atlas');
+			links = mongo.db('Links').collection('Links');
+		}
+
+		let url = new URL(req.url);
+
+		switch(req.method){
+			case 'GET':
+				if(url.pathname === '/')
+					return new Response(homepage, { headers: { 'Content-Type': 'text/html' } });
+
+				let link = await links.findOne({ url: url.pathname });
+				if(!link)
+					return new Response(homepage, { status: 404, headers: { 'Content-Type': 'text/html' } });
+
+				if(link.client)
+					return new Response('<script>window.location = \'' + link.redirect.replace('\'', '"') + '\'</script>', { headers: { 'Content-Type': 'text/html' }});
+				else
+					return new Response(null, { status: 307, headers: { location: link.redirect } });
+			case 'PUT':
+				if(url.pathname === '/api/v1/link'){
+					let isClient = req.headers.get('client') === 'yes';
+					let link = await links.findOne({ redirect: req.headers.get('url'), client: isClient });
+
+					if(link)
+						return new Response(JSON.stringify({ ok: true, url: link.url }));
+
+					let linkID = Math.random().toString(36).replace('0.', '');
+
+					link = {
+						_id: linkID,
+						url: '/itm/'+linkID,
+						redirect: req.headers.get('url'),
+						client: req.headers.get('client') === 'yes'
+					}
+
+					await links.insertOne(link);
+					return new Response(JSON.stringify({ ok: true, url: link.url }));
+				}
+			default:
+				return new Response(homepage, { status: 404, headers: { 'Content-Type': 'text/html' } });
+		}
 	},
 };
